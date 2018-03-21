c5ff4d0e87438d52daf3354528a9e08c1eaed92c
文信周 <wenxinzhou@cvte.com>
2018-03-21 12:58:45 +0800

    更新pb生成脚本

--- win-client/tools/protobuf-2.6.1/make_PB_Files.bat
+++ win-client/tools/protobuf-2.6.1/make_PB_Files.bat
@@ -4,7 +4,7 @@ cls
 COLOR 1f
 
 ECHO.
-ECHO.
+ECHO.  
 ECHO   ##############################################################
 ECHO   #               欢迎使用 TeamTalk 工程配置向导               #
 ECHO   #                   version 1.0                              #
@@ -15,10 +15,10 @@ ECHO.
 rem 拷贝PB协议文件
 echo make IM protocol buffer files...
 cd %~dp0
-mkdir %~dp0\..\..\include\ProtocolBuffer
-copy  %~dp0\..\..\..\pb\*.proto %~dp0\*.proto
+mkdir %~dp0..\..\include\ProtocolBuffer
+xcopy %~dp0..\..\..\pb\*.proto %~dp0\proto\*.proto
 echo Copy IM protocol buffer files finished
 rem 生成PB协议的C++版本
-protoc.exe --cpp_out=%~dp0\..\..\include\ProtocolBuffer *.proto
+protoc.exe --proto_path=%~dp0proto\ --cpp_out=%~dp0..\..\include\ProtocolBuffer %~dp0proto\*.proto
 echo make the pb files of c++ version has finished.
 pause
