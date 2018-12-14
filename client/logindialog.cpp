535117c3757454a78fa44125e6fccdd8a1a830f0
My Name <my-name@chromium.org>
2018-12-14 22:56:57 +0800

    登录界面

--- client/logindialog.cpp
+++ client/logindialog.cpp
@@ -1,11 +1,25 @@
 #include "logindialog.h"
 #include "ui_logindialog.h"
+#include <QFile>
+#include <QTextStream>
+#include <QDebug>
 
 LoginDialog::LoginDialog(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::LoginDialog)
 {
     ui->setupUi(this);
+
+    setWindowFlags(Qt::FramelessWindowHint);
+
+    QFile file(":/resource/qss/LoginDialog.qss");
+    file.open(QFile::ReadOnly);
+    QTextStream filetext(&file);
+    QString stylesheet = filetext.readAll();
+    setStyleSheet(stylesheet);
+    file.close();
+
+    qDebug() << stylesheet;
 }
 
 LoginDialog::~LoginDialog()
